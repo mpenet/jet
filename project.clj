4696b54850827173b0f0ed4e95bcf9f441fd0c50
Max Penet <m@qbits.cc>
2014-08-06 13:14:03 +0200

    drop tools.logging dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [ring/ring-core "1.3.0"
                   :exclusions [javax.servlet/servlet-api]]
