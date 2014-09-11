a6b08376282a12ef2c74911cfaaffc04329aa863
Max Penet <m@qbits.cc>
2014-09-11 08:12:38 +0200

    add more test profiles

--- project.clj
+++ project.clj
@@ -11,15 +11,22 @@
                  [org.eclipse.jetty.websocket/websocket-client "9.2.3.v20140905"]
                  [org.eclipse.jetty/jetty-client "9.2.3.v20140905"]
                  [cheshire "5.3.1"]]
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :dev  {:dependencies [[codox "0.8.10"]]}
-             :test  {:dependencies [[ring/ring-core "1.3.0"
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
+             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :dev  {:dependencies [[ring/ring-core "1.3.0"
                                      :exclusions [javax.servlet/servlet-api]]
                                     [ring/ring-servlet "1.3.0"
                                      :exclusions [javax.servlet/servlet-api]]
-]}}
+                                    [codox "0.8.10"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,1.7:dev,master"]}
   :codox {:src-dir-uri "https://github.com/mpenet/jet/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-dir "doc"
