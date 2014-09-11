4589d68789da38bc7f5cbeb335e76157c23f3ebd
Max Penet <m@qbits.cc>
2014-09-11 08:04:38 +0200

    missed one artifact

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [org.eclipse.jetty/jetty-server "9.2.2.v20140723"]
+                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
                  [org.eclipse.jetty.websocket/websocket-server "9.2.3.v20140905"]
                  [org.eclipse.jetty.websocket/websocket-servlet "9.2.3.v20140905"]
                  [org.eclipse.jetty.websocket/websocket-client "9.2.3.v20140905"]
