cadaf03402f4e88104a47e8628a943bb416d0361
Max Penet <m@qbits.cc>
2014-09-26 10:48:50 +0200

    use latest core async

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
                  [org.eclipse.jetty.websocket/websocket-server "9.2.3.v20140905"]
                  [org.eclipse.jetty.websocket/websocket-servlet "9.2.3.v20140905"]
