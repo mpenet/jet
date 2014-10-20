a4dd1229891db14014f9c02bc51d00b4c1ad4b49
Max Penet <m@qbits.cc>
2014-10-20 13:06:45 +0200

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/jet "0.5.0-beta1"
+(defproject cc.qbits/jet "0.5.0-SNAPSHOT"
   :description ""
   :url "https://github.com/mpenet/jet"
   :license {:name "Eclipse Public License"
