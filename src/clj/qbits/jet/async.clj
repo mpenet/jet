4f2442cfdd51974c15bda4e8379dfaf7d45989cc
Max Penet <m@qbits.cc>
2015-01-06 15:26:28 +0100

    fix overzealous suspend!

--- src/clj/qbits/jet/async.clj
+++ src/clj/qbits/jet/async.clj
@@ -2,18 +2,26 @@
   (:require [clojure.core.async :as async]))
 
 (defn put!
-  "Takes a `ch`, a `msg`, a fn that enables backpressure, one that disables it,
-  and a no-arg function which, when invoked, closes the upstream source."
-  ([ch msg suspend! resume! close!]
+  "Takes a `ch`, a `msg`, a fn that enables backpressure, one that
+  disables it, a success callback and a no-arg function which, when
+  invoked, closes the upstream source."
+  ([ch msg suspend! resume! success! close!]
    (let [status (atom ::sending)]
      (async/put! ch msg
                  (fn [result]
                    (if-not result
-                     (when (fn? close!) (close!))
-                     (when (compare-and-set! status ::paused  ::sent)
-                       (resume!)))))
+                     (when close! (close!))
+                     (do
+                       (when (identical? @status ::paused)
+                         ;; if it was paused resume
+                         (resume!))
+                       (reset! status ::sent)
+                       (when success! (success!))))))
+     ;; it's still sending, means it's parked, so suspend source
      (when (compare-and-set! status ::sending ::paused)
        (suspend!))
      nil))
+  ([ch msg suspend! resume! success!]
+   (put! ch msg suspend! resume! success! nil))
   ([ch msg suspend! resume!]
-   (put! ch msg suspend! resume! nil)))
+   (put! ch msg suspend! resume! nil nil)))
