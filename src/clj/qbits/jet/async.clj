0590ab3bba1b1c0eb271a51f9cd19b52467271d8
Max Penet <m@qbits.cc>
2015-01-07 09:10:13 +0100

    fix race condition, thanks ztellman

--- src/clj/qbits/jet/async.clj
+++ src/clj/qbits/jet/async.clj
@@ -2,19 +2,21 @@
   (:require [clojure.core.async :as async]))
 
 (defn put!
-  "Takes a `ch`, a `msg`, a fn that enables backpressure, one that
-  disables it and a no-arg function which, when
-  invoked, closes the upstream source."
+  "Takes a `ch`, a `msg`, a single arg function that when passed
+   `true` enables backpressure and when passed `false` disables it,
+   and a no-arg function which, when invoked, closes the upstream
+   source."
   ([ch msg backpressure! close!]
    (let [status (atom ::sending)]
      (async/put! ch msg
                  (fn [result]
                    (if-not result
                      (when close! (close!))
-                     (do
-                       (if (compare-and-set! status ::paused ::sent)
-                         (backpressure! false)
-                         (reset! status ::sent))))))
+                     (cond
+                       (compare-and-set! status ::sending ::sent)
+                       nil
+                       (compare-and-set! status ::paused  ::sent)
+                       (backpressure! false)))))
      ;; it's still sending, means it's parked, so suspend source
      (when (compare-and-set! status ::sending ::paused)
        (backpressure! true))
